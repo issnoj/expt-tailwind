b7ca5eb5ba4af1036b19bb3d9d4a6b8a20a6013b
issnoj <issei.nojiri@gmail.com>
2024-02-14 18:35:59 +0900

    [storybook] add coverage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,6 +15,7 @@ const config: StorybookConfig = {
     '@storybook/addon-themes',
     '@storybook/addon-a11y',
     '@storybook/addon-interactions',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/nextjs',
