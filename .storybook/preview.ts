d59652e1cf4f025dcaeea9aecf5ee148e534806d
issnoj <issei.nojiri@gmail.com>
2024-02-14 11:15:51 +0900

    integrate storybook with tailwind

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,27 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react';
+import '../app/globals.css';
+import { withThemeByClassName } from '@storybook/addon-themes';
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
 };
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+];
 
 export default preview;
