8fa2a4abd009d7a0f18ff313f88bb5483823a4c8
issnoj <issei.nojiri@gmail.com>
2024-03-04 10:41:08 +0900

    add motion hamburger-menu

---
+++ components/motion/hamburger-menu.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useState } from 'react';
+import { HamburgerButton } from '@/components/ui/hamburger-button';
+
+export const HamburgerMenu = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <HamburgerButton
+      className={'bg-secondary text-blue-500'}
+      open={open}
+      onClick={handleClick}
+    />
+  );
+};
--- components/motion/motion-page.tsx
+++ components/motion/motion-page.tsx
@@ -7,6 +7,8 @@ import { Sample } from '@/components/sample';
 import { Link } from '@/components/ui/link';
 import { EnterText } from '@/components/motion/enter-text';
 import { StaggerMenu } from '@/components/motion/stagger-menu';
+import { HamburgerMenu } from '@/components/motion/hamburger-menu';
+import { VariantLabels, Variants } from 'framer-motion';
 
 export const MotionPage = () => {
   return (
@@ -137,6 +139,116 @@ export const HeartButton = () => {
         }
       >
         <StaggerMenu />
+      </Sample>
+      <Sample
+        code={`
+const Line = ({
+  width,
+  y,
+  variants,
+  animate,
+}: {
+  width: number;
+  y: number;
+  variants: Variants;
+  animate: VariantLabels;
+}) => (
+  <motion.line
+    x1={0}
+    x2={width}
+    y1={y}
+    y2={y}
+    strokeWidth={3}
+    fill="transparent"
+    stroke="currentColor"
+    strokeLinecap="round"
+    variants={variants}
+    animate={animate}
+  />
+);
+
+type Props = {
+  open: boolean;
+  onClick: () => void;
+  className?: string;
+  width?: number;
+  height?: number;
+};
+
+export const HamburgerButton = ({
+  open,
+  onClick,
+  className,
+  width = 24,
+  height = 18,
+}: Props) => {
+  const variant = open ? 'opened' : 'closed';
+  const gap = height / 2;
+
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        'grid size-[40px] place-content-center text-primary',
+        className,
+      )}
+    >
+      <svg
+        viewBox={\`0 0 \${width} \${height}\`}
+        width={width}
+        height={height}
+        overflow={'visible'}
+      >
+        <Line
+          width={width}
+          y={0}
+          animate={variant}
+          variants={{
+            closed: {
+              rotate: 0,
+              translateY: 0,
+            },
+            opened: {
+              rotate: 45,
+              translateY: gap,
+            },
+          }}
+        />
+        <Line
+          width={width}
+          y={gap}
+          animate={variant}
+          variants={{
+            closed: {
+              opacity: 1,
+            },
+            opened: {
+              opacity: 0,
+            },
+          }}
+        />
+        <Line
+          width={width}
+          y={gap * 2}
+          animate={variant}
+          variants={{
+            closed: {
+              rotate: 0,
+              translateY: 0,
+            },
+            opened: {
+              rotate: -45,
+              translateY: -gap,
+            },
+          }}
+        />
+      </svg>
+    </button>
+  );
+};
+`}
+      >
+        <HamburgerMenu />
       </Sample>
     </Content>
   );
