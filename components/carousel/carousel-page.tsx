3728de6235d938db2b6049ee165bff66b481188d
issnoj <issei.nojiri@gmail.com>
2024-03-21 20:25:08 +0900

    add img slide carousel

--- components/carousel/carousel-page.tsx
+++ components/carousel/carousel-page.tsx
@@ -4,12 +4,16 @@ import React from 'react';
 import { Content } from '@/components/ui/content';
 import { ImgGridCarouselSample } from '@/components/carousel/img-grid-carousel-sample';
 import { Sample } from '@/components/sample';
+import { ImgSlideCarouselSample } from '@/components/carousel/img-slide-carousel-sample';
 
 export const CarouselPage = () => {
   return (
     <Content title={'カルーセル'}>
       <Sample>
         <ImgGridCarouselSample />
+      </Sample>
+      <Sample>
+        <ImgSlideCarouselSample />
       </Sample>
     </Content>
   );
